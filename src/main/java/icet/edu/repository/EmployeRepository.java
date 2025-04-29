0e99feb2b46716eee923f7cb7c4517ee8ba01a45
samitha senewirathna <samithani17@gmail.com>
2025-04-29 11:12:12 +0530

    implement repository layer

--- src/main/java/icet/edu/repository/EmployeRepository.java
+++ src/main/java/icet/edu/repository/EmployeRepository.java
@@ -1,4 +1,11 @@
 package icet.edu.repository;
+import icet.edu.entity.EmployeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeRepository {
+import java.util.List;
+
+public interface EmployeRepository extends JpaRepository<EmployeEntity, Long> {
+
+List<EmployeEntity> findByName(String name);
+
 }
--- src/main/java/icet/edu/service/impl/EmployeServiceImpl.java
+++ src/main/java/icet/edu/service/impl/EmployeServiceImpl.java
@@ -1,4 +1,44 @@
 package icet.edu.service.impl;
 
-public class EmployeServiceImpl {
+import icet.edu.dto.Employe;
+import icet.edu.repository.EmployeRepository;
+import icet.edu.service.EmployeService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class EmployeServiceImpl implements EmployeService {
+
+
+    final EmployeRepository repository;
+    final ModelMapper mapper;
+
+    @Override
+    public void addEmploye(Employe employe) {
+
+    }
+
+    @Override
+    public void deleteEmploye(Integer id) {
+
+    }
+
+    @Override
+    public void updateEmploye() {
+
+    }
+
+    @Override
+    public List<Employe> getEmployes() {
+        return List.of();
+    }
+
+    @Override
+    public List<Employe> searchByName(String name) {
+        return List.of();
+    }
 }
